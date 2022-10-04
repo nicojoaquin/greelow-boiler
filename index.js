795ebbdfc09303d1bcc1f519aff1e029d24a953f
nicojoaquin <nicolas.joaquin@greelow.com>
2022-10-04 13:03:09 -0300

    starting package

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+
+const spawn = require("child_process").spawn;
+const params = ["pathToScript", "run", "-silent"];
+spawn("bash", params, {
+  stdio: "ignore",
+  detached: true,
+}).unref();
