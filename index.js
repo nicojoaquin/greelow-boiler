0d10dc1a0fb3057cb8a9fc289fbfc9fb4f5b600e
nicojoaquin <nicolas.joaquin@greelow.com>
2022-10-06 10:46:30 -0300

    bin command

--- index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const spawn = require("child_process").spawn;
-const params = ["pathToScript", "run", "-silent"];
-spawn("bash", params, {
-  stdio: "ignore",
-  detached: true,
-}).unref();
